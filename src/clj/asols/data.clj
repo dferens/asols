213248ef96f655a1e62dd13d99d5939ec2df0b8e
Ferens Dmitriy <ferensdima@gmail.com>
2015-04-24 16:28:06 +0300

    Added monks-2 and monks-3 datasets

--- src/clj/asols/data.clj
+++ src/clj/asols/data.clj
@@ -25,8 +25,10 @@
                  (entry [0 1] [0 1])]]
     (->Dataset entries entries 2 2)))
 
-(def monks1
+(defn parse-monks-dataset
   "
+  All monks datasets has next attributes:
+
   Input vector:
     1. a1:    1, 2, 3
     2. a2:    1, 2, 3
@@ -36,10 +38,10 @@
     6. a6:    1, 2
 
   Output vector:
-    1. class: 0, 1 : (a1 = a2) or (a5 = 1)
+    1. class: 0, 1
   "
-  (let [files ["monks-1_learn.tab" "monks-1_test.tab"]
-        read-file
+  [train-file test-file]
+  (let [read-file
         (fn [path]
           (for [line (drop 3 (read-dataset path))]
             (let [parsed-line (map #(Integer/parseInt %) (butlast line))
@@ -47,5 +49,21 @@
                   out-vec (if (zero? class-label) [1 0] [0 1])
                   in-vec (rest parsed-line)]
               (entry in-vec out-vec))))
-        [train test] (map (comp vec read-file) files)]
+        [train test] (map (comp vec read-file) [train-file test-file])]
     (->Dataset train test 6 2)))
+
+(def monks1
+  "Problem:
+    (a1 = a2) or (a5 = 1)"
+  (parse-monks-dataset "monks-1_learn.tab" "monks-1_test.tab"))
+
+(def monks2
+  "Problem:
+    EXACTLY TWO of {a1 = 1, a2 = 1, a3 = 1, a4 = 1, a5 = 1, a6 = 1}"
+  (parse-monks-dataset "monks-2_learn.tab" "monks-2_test.tab"))
+
+(def monks3
+  "Problem:
+    (a5 = 3 and a4 = 1) or (a5 /= 4 and a2 /= 3)
+    (5% class noise added to the training set)"
+  (parse-monks-dataset "monks-3_learn.tab" "monks-3_test.tab"))
