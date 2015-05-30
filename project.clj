e0236cbec23a8f0ab5db864d90c8120cf53cff7e
Ferens Dmitriy <ferensdima@gmail.com>
2015-05-30 18:09:18 +0300

    Clojure 1.7 !!!

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/clojurescript "0.0-3269"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.omcljs/om "0.8.8"]
@@ -13,7 +13,7 @@
                  [com.taoensso/timbre "3.4.0"]
                  [net.mikera/vectorz-clj "0.29.0"]
                  [net.mikera/core.matrix.stats "0.5.0"]
-                 [compojure "1.3.1"]
+                 [compojure "1.3.4"]
                  [dorothy "0.0.6"]
                  [figwheel "0.2.6"]
                  [incanter "1.5.6"]
