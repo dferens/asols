48371f17ba9cd34c56b513c97adf3c471d047825
Dmitriy Ferens <ferensdima@gmail.com>
2015-07-27 20:07:52 +0300

    Fixed exception on headless environment

--- src/clj/asols/core.clj
+++ src/clj/asols/core.clj
@@ -1,5 +1,6 @@
 (ns asols.core
   (:gen-class :main true)
+  (:import java.awt.HeadlessException)
   (:require [clojure.java.browse :refer [browse-url]]
             [asols.server :as server]))
 
@@ -9,4 +10,6 @@
         port (Integer/parseInt (or (second args) "8000"))]
     (server/run :ip ip :port port)
     (prn "Server started")
-    (browse-url (str "http://" ip ":" port))))
+    (try
+      (browse-url (str "http://" ip ":" port))
+      (catch HeadlessException _))))
